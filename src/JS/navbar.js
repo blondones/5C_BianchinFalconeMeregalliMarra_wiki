d190379b259b9daf923c61efe9b93af0a8e58dd2
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-03-31 13:49:34 +0200

    sistemazione pagine

---
+++ src/JS/navbar.js
@@ -0,0 +1,7 @@
+
+fetch('../HTML/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Errore nel caricare la navbar:', error));

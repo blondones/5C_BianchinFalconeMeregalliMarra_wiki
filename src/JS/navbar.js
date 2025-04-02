9f3188ae03080bc79315264a27352afb82595fe2
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-04-02 09:45:16 +0200

    creazione multi navbar e allocazione

--- src/JS/navbar.js
+++ src/JS/navbar.js
@@ -1,7 +1,35 @@
 
+// Prende il valore dal data-navbar
+function getNavbarType() {
+    var container = document.getElementById("navbar-container");
+    return container ? container.getAttribute("data-navbar") : "navbar-search"; // Default se non è specificato
+}
+
+// Carica dinamicamente la navbar
 fetch('../HTML/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-container').innerHTML = data;
+
+        // Dopo il caricamento, mostra solo la navbar scelta
+        mostraNavbar(getNavbarType());
     })
     .catch(error => console.error('Errore nel caricare la navbar:', error));
+
+// Funzione per mostrare solo la navbar selezionata
+function mostraNavbar(tipo) {
+    var navbars = ["navbar-search", "navbar-logout", "navbar-welcome", "navbar-empty"];
+
+    for (var i = 0; i < navbars.length; i++) {
+        var navbar = document.getElementById(navbars[i]);
+        if (navbar) {
+            navbar.style.display = "none";
+        }
+    }
+
+    var navbarDaMostrare = document.getElementById(tipo);
+    if (navbarDaMostrare) {
+        navbarDaMostrare.style.display = "block";
+    }
+}
+

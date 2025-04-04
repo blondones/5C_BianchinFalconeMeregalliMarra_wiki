ca5b56c61d54c40a74a5363ade3f600fb6e0cde5
AndreaFalcone1 <falconeandrea@itis-molinari.eu>
2025-04-04 08:51:33 +0200

    Impostazione Logica Register

--- src/PAGINE/register.php
+++ src/PAGINE/register.php
@@ -15,9 +15,7 @@
     <div id="register-container">
         <h1>REGISTER</h1>
         <form id="formRegister" action="" method="POST">
-            
-            <input type="text" class="inputLogin" id="inputNome" placeholder="Inserisci la tuo nome" required> <!-- inserimento nome -->
-            <input type="email" class="inputLogin" id="inputEmail" placeholder="Inserisci la tua email" required> <!-- inserimento email -->
+            <input type="email" class="inputLogin" id="email" placeholder="Inserisci la tua email" required> <!-- inserimento email -->
             <input type="password" class="inputLogin" id="password" placeholder="Inserisci la tua password" required> <!-- inserimento password -->
             <input type="password" class="inputLogin" id="confermaPassword" placeholder="Conferma la tua password" required> <!-- conferma password -->
             <button id="bottoneWriter" type="submit">Register as Writer</button>

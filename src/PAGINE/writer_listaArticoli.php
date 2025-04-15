328ed354e17dbb88204d1307d62751baa568c43f
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-04-15 11:52:45 +0200

    creazione file listarticoli 1.2

--- src/PAGINE/writer_listaArticoli.php
+++ src/PAGINE/writer_listaArticoli.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>listaArticoliBozze</title>
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
+
 <body>
-    <!--NAVBAR-->
+    <!-- NAVBAR -->
     <div id="navbar-container" data-navbar="navbar-logout-WR"></div>
 
     <h1>Article List</h1>
@@ -24,13 +26,13 @@
         </div>
     </form>
 
-    <!-- Bottone Create New in basso al centro -->
     <div class="create-new-container">
-        <form action="writerListaArticoli.php" method="POST">
+        <form action="../PAGINE/writer_creazioneArticolo.php" method="POST">
             <button type="submit">Create New</button>
         </form>
     </div>
 
     <script src="../JS/navbar.js"></script>
 </body>
+
 </html>

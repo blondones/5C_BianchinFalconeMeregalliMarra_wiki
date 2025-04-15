33d5871b0bdd67a0aa960091a0d570b74f9175e8
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-04-15 11:41:12 +0200

    creazione file listarticoli 1.0

--- src/PAGINE/writer_listaArticoli.php
+++ src/PAGINE/writer_listaArticoli.php
@@ -10,18 +10,27 @@
     <!--NAVBAR-->
     <div id="navbar-container" data-navbar="navbar-logout-WR"></div>
 
-
     <h1>Article List</h1>
-    <div class="role-container">
-        <div class="role-info">
-            <p>Articolo N</p>
+
+    <form action="writerListaArticoli.php" method="POST">
+        <div class="role-container">
+            <div class="role-info">
+                <label for="articoloN">Articolo N</label>
+                <input type="text" id="articoloN" name="articoloN" required>
+            </div>
+            <div class="role-buttons">
+                <button type="submit" name="action" value="modify">Modify</button>
+            </div>
         </div>
-        <div class="role-buttons">
-            <button id="bottone">Check</button>
-            
-        </div>
+    </form>
+
+    <!-- Bottone Create New in basso al centro -->
+    <div class="create-new-container">
+        <form action="writerListaArticoli.php" method="POST">
+            <input type="hidden" name="action" value="create">
+            <button type="submit">Create New</button>
+        </form>
     </div>
-    
 
     <script src="../JS/navbar.js"></script>
 </body>
